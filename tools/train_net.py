0c10e36e92cf87edf16e8b8be33e57aa6db6f1de
Michele Tanus <michele.silva@gmail.com>
2020-02-11 11:55:08 -0300

    FIX: Forcing to use all available GPUs

--- tools/train_net.py
+++ tools/train_net.py
@@ -9,7 +9,6 @@ from maskrcnn_benchmark.utils.env import setup_environment  # noqa F401 isort:sk
 
 import argparse
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2'
 import torch
 from maskrcnn_benchmark.config import cfg
 from maskrcnn_benchmark.data import make_data_loader
